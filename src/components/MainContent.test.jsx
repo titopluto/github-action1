9de93390e1ed07c20acbfcc644e8df7fb69e094c
Tito <titocopie1@gmail.com>
2023-02-24 13:32:31 -0400

    reusable workflow

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-area-mistake')).toBeInTheDocument();
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
